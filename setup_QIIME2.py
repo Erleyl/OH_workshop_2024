f95d86fec009d82f47f8fd78b8bcb4949260d8d7
Erley F. Lizarazo Forero <32706783+Erleyl@users.noreply.github.com>
2024-07-30 13:15:27 +0200

    Update setup_QIIME2.py

--- setup_QIIME2.py
+++ setup_QIIME2.py
@@ -5,6 +5,7 @@ Only for Google Colab
 
 import os
 import sys
+import shutil
 from subprocess import Popen, PIPE
 
 r = Popen(["pip", "install", "rich"])
@@ -12,26 +13,51 @@ r.wait()
 from rich.console import Console  # noqa
 con = Console()
 
-has_conda = "conda version" in os.popen("conda info").read()
+PREFIX = "/usr/local/miniforge3/"
+
+has_conda = "conda version" in os.popen("%s/bin/conda info" % PREFIX).read()
 has_qiime = "QIIME 2 release:" in os.popen("qiime info").read()
 
 
 MINICONDA_PATH = (
-    "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+    "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh"
 )
-QIIME_YAML_URL = (
-    "https://data.qiime2.org/distro/amplicon/qiime2-amplicon-2024.5-py39-linux-conda.yml"
+
+QIIME_YAML_TEMPLATE = (
+    "https://data.qiime2.org/distro/amplicon/qiime2-amplicon-{version}-py{python}-linux-conda.yml"
 )
+
+if len(sys.argv) == 2:
+    version = sys.argv[1]
+else:
+    version = "2024.5"
+
+if tuple(float(v) for v in version.split(".")) < (2021, 4):
+    pyver = "36"
+else:
+    pyver = "39"
+
+CONDA = "mamba"
+CONDA_ARGS = ["-q"]
+
+if tuple(float(v) for v in version.split(".")) < (2023, 9):
+    QIIME_YAML_TEMPLATE = (
+        "https://data.qiime2.org/distro/core/qiime2-{version}-py{python}-linux-conda.yml"
+    )
+
+QIIME_YAML_URL = QIIME_YAML_TEMPLATE.format(version=version, python=pyver)
 QIIME_YAML = os.path.basename(QIIME_YAML_URL)
 
 
 def cleanup():
     """Remove downloaded files."""
-    if os.path.exists("Miniconda3-latest-Linux-x86_64.sh"):
-        os.remove("Miniconda3-latest-Linux-x86_64.sh")
+    if os.path.exists(os.path.basename(MINICONDA_PATH)):
+        os.remove(os.path.basename(MINICONDA_PATH))
     if os.path.exists(QIIME_YAML):
         os.remove(QIIME_YAML)
-    con.log("Cleaned up unneeded files.")
+    if os.path.exists("/content/sample_data"):
+        shutil.rmtree("/content/sample_data")
+    con.log(":broom: Cleaned up unneeded files.")
 
 
 def run_and_check(args, check, message, failure, success, console=con):
@@ -49,37 +75,25 @@ def run_and_check(args, check, message, failure, success, console=con):
         sys.exit(1)
 
 
-def _hack_in_the_plugins():
-    """Add the plugins to QIIME."""
-    import qiime2.sdk as sdk
-    from importlib.metadata import entry_points
-
-    pm = sdk.PluginManager(add_plugins=False)
-    for entry in entry_points()["qiime2.plugins"]:
-        plugin = entry.load()
-        package = entry.value.split(':')[0].split('.')[0]
-        pm.add_plugin(plugin, package, entry.name)
-
-
 if __name__ == "__main__":
     if not has_conda:
         run_and_check(
             ["wget", MINICONDA_PATH],
             "saved",
-            ":snake: Downloading miniconda...",
-            "failed downloading miniconda :sob:",
+            ":snake: Downloading miniforge...",
+            "failed downloading miniforge :sob:",
             ":snake: Done."
         )
 
         run_and_check(
-            ["bash", "Miniconda3-latest-Linux-x86_64.sh", "-bfp", "/usr/local"],
+            ["bash", os.path.basename(MINICONDA_PATH), "-bfp", PREFIX],
             "installation finished.",
-            ":snake: Installing miniconda...",
-            "could not install miniconda :sob:",
-            ":snake: Installed miniconda to `/usr/local` :snake:"
+            ":snake: Installing miniforge...",
+            "could not install miniforge :sob:",
+            ":snake: Installed miniforge to `/usr/local`."
         )
     else:
-        con.log(":snake: Miniconda is already installed. Skipped.")
+        con.log(":snake: Miniforge is already installed. Skipped.")
 
     if not has_qiime:
         run_and_check(
@@ -91,12 +105,19 @@ if __name__ == "__main__":
         )
 
         run_and_check(
-            ["conda", "env", "update", "-n", "base", "--file",
-             "qiime2-2021.4-py38-linux-conda.yml"],
-            "To activate this environment, use",
+            [PREFIX + "bin/" + CONDA, "env", "create", *CONDA_ARGS, "--prefix", "/usr/local", "--file", QIIME_YAML],
+            "Verifying transaction: ...working... done",
             ":mag: Installing Qiime 2. This may take a little bit.\n :clock1:",
             "could not install Qiime 2 :sob:",
             ":mag: Done."
+        )
+
+        run_and_check(
+            ["pip", "install", "empress"],
+            "Successfully installed empress-",
+            ":evergreen_tree: Installing Empress...",
+            "could not install Empress :sob:",
+            ":evergreen_tree: Done."
         )
     else:
         con.log(":mag: Qiime 2 is already installed. Skipped.")
@@ -109,8 +130,8 @@ if __name__ == "__main__":
         ":bar_chart: Qiime 2 command line looks good :tada:"
     )
 
-    if sys.version_info[0:2] == (3, 8):
-        sys.path.append("/usr/local/lib/python3.8/site-packages")
+    if sys.version_info[0:2] == (int(pyver[0]), int(pyver[1])):
+        sys.path.append("/usr/local/lib/python3.{}/site-packages".format(pyver[1]))
         con.log(":mag: Fixed import paths to include Qiime 2.")
 
         con.log(":bar_chart: Checking if Qiime 2 import works...")
@@ -120,15 +141,6 @@ if __name__ == "__main__":
             con.log("[red]Qiime 2 can not be imported :sob:[/red]")
             sys.exit(1)
         con.log("[blue]:bar_chart: Qiime 2 can be imported :tada:[/blue]")
-
-        con.log(":bar_chart: Setting up QIIME 2 plugins...")
-        try:
-            _hack_in_the_plugins()
-            from qiime2.plugins import feature_table # noqa
-        except Exception:
-            con.log("[red]Could not add the plugins :sob:[/red]")
-            sys.exit(1)
-        con.log("[blue]:bar_chart: Plugins are working :tada:[/blue]")
 
     cleanup()
 
